a86134d44a62da6bafe7a14abaa98d01cba25529
karolin siebert <karolin@intellisense.io>
2018-03-11 07:39:14 +0100

    apply Bellman-Ford algorithm to optimisation task

--- app/trains.js
+++ app/trains.js
@@ -3,15 +3,14 @@
 class TripPlanner {
     constructor (connections) {
         this.connectionsStrings = connections;
-
         this.parseConnectionStrings();
     }
 
     /**
-    * Take strings of city codes and distances and create and array of objects for all connections 
+    * Take strings of city codes and distances and create and array of objects for all connections
     */
     parseConnectionStrings () {
-        this.connectionList = this.connectionsStrings.map(string => {
+        this.connectionGraph = this.connectionsStrings.map(string => {
             const startCity = string[0];
             const stopCity = string[1];
             const distance = string.slice(2, string.length);
@@ -24,7 +23,7 @@ class TripPlanner {
     }
 
     /**
-    * Create pairs of letters that stand for cities for each connection 
+    * Create pairs of letters that stand for cities for each connection
     * @param {array} Array of all possible connections
     * @return {array} Array of pairs of actual connections
     */
@@ -39,7 +38,7 @@ class TripPlanner {
     }
 
     /**
-    * Calculating distance of trips 
+    * Calculating distance of trips
     * @param {string} String of city letters
     * @return {number} Total distance covered
     */
@@ -49,7 +48,7 @@ class TripPlanner {
         let totalDistance = 0;
         cityPair.forEach(pair => {
             //find distance in list of all connections
-            this.connectionList.find(departure => {
+            this.connectionGraph.find(departure => {
                 if (departure.from === pair[0] && departure.to === pair[1]){
                     totalDistance += departure.distance;
                 }
@@ -65,34 +64,39 @@ class TripPlanner {
     }
 
     findShortestTrip (route){
-        //const cache = new TripCache();
         /**
-        * Since we don't know what the stops in between are we just 
-        * have an start and an end city
-        */
-        const start = route[0];
-        const end = route[1];
+         * This is a JS implementation of the Bellman-Ford Algorithm
+         * The idea is to find the shortest connections for each inner one 
+         * that then gets summed up
+         */
 
-        const departingCities = [];
-        const arrivingCities = [];
-        this.connectionList.forEach(connection => {
-            if (connection.from === start){
-                departingCities.push(connection);
-            } else if (connection.to === end){
-                arrivingCities.push(connection);
-            }
-        });
+        const memo = {
+            A: 0,
+            B: Number.POSITIVE_INFINITY,
+            C: Number.POSITIVE_INFINITY,
+            D: Number.POSITIVE_INFINITY,
+            E: Number.POSITIVE_INFINITY
+        };
 
-        const distances = [];
-        departingCities.forEach((city, i) => {
-            arrivingCities.forEach(arrivalCity => {
-                if (city.to === arrivalCity.from){
-                    distances.push(city.distance + arrivalCity.distance);
-                }
+        const cities = ["A", "B", "C", "D", "E"];
+
+        for (const city of cities){
+            //getting real connections from all available connections per city
+            const realConn = this.connectionGraph.filter((conn) => {
+                return conn.from === city;
             });
-        });
-        
-        return Math.min(...distances);
+
+            for (const conn of realConn){
+                const potentialDistance = memo[conn.from] + conn.distance;
+
+                if (potentialDistance < memo[conn.to]){
+
+                    memo[conn.to] = potentialDistance;
+
+                }
+            }
+        }
+        return memo["C"];
 
     }
 
@@ -100,17 +104,20 @@ class TripPlanner {
 
     }
 }
-
+/*
 class TripCache {
     constructor (){
-        this.table = [{city: "A", distance: 0, visited: false}];
+        this.distanceObject = {
+            A: 0,
+            B: 0,
+            C: 0,
+            D: 0,
+            E: 0
+        };
 
     }
 
-    
-}
+} */
 
-/* const trainConnections = ["AB5", "BC4", "CD8", "DC8", "DE6", "AD5", "CE2", "EB3", "AE7"];
-new TripPlanner(trainConnections); */
 
 module.exports.TripPlanner = TripPlanner;
